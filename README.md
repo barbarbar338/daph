e416aef9e0ffcea5600122db45b88053ebdba143
barbarbar338 <demirci.baris38@gmail.com>
2020-11-24 10:50:19 +0300

    🦄 Init

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# daph
-✨ A simple CLI commander system
+# 🦄 Daph
+- A simple CLI commander system ✨
+
+# 📥 Installation
+Using yarn:
+```
+$ yarn add daph
+```
+Using npm:
+```
+$ npm install daph
+```
+
+# 🔧 Usage
+```ts
+daph
+    .createCommand(ICommand, callback(commandName, args) => unknown) // create command
+    .createCommand(ICommand, callback(commandName, args) => unknown) // create another command
+    .help() // If none of the above commands are used, execute the help command.
+```
+- `ICommand`: Where command is defined to be used when separating arguments. Structure:
+```js
+{
+    name: "test", // Command name
+    usage: "--message <your_message>", // Command usage
+    example: [ "--message Hello, world!", "-m Test message" ], // Usage examples
+    category: "test", // Command category
+    aliases: [ "t" ], // Command aliases
+    description: "just a test command", // Command description
+    argDefinitions: [
+        { name: "message", type: String, aliases: [ "m" ] }
+    ] // Arg definition array (Learn about ArgDefinition below)
+}
+```
+- `ArgDefinition`: Where options are defined to be used when separating arguments. Structure:
+```js
+{
+    name: "message", // Option name
+    type: String // Option type (function)
+    aliases?: [ "m" ], // Option aliases (optional)
+    default?: false, // optional
+    isOptional?: false // optional
+}
+```
+- `callback(commandName, args) => unknown`: If the command you set is used, the action to be applied. Example:
+```js
+daph //         callback(commandName, args) ⬇️
+    .createCommand(ICommand, (commandName, args) => {
+        console.log("You used", commandName, "command with arguments", args);
+    });
+```
+
+# 🛠️ Example
+```js
+import daph from "daph";
+
+daph
+    .createCommand({
+        name: "install",
+        usage: "<module_name> [--version] <package_version>",
+        example: [ "daph", "bargs --version 1.0.1" ],
+        category: "utility",
+        aliases: [ "i", "add" ],
+        description: "Install packages from server",
+        argDefinitions: [
+            { name: "module", type: String, default: true },
+            { name: "version", type: String, isOptional: true }
+        ]
+    }, (commandName, args) => {
+        const { module, version } = args;
+        console.log("You have downloaded package", module, "with version" version ? version : "latest");
+    })
+    .help()
+```
+
+# 🔗 Contributing / Issues / Ideas
+Feel free to use GitHub's features ✨
---
+++ src/index.ts
@@ -0,0 +1,87 @@
+import { bargs } from "bargs";
+import {
+    BargsResult,
+    DaphFunction,
+    HelpCategory,
+    ICommand,
+    UnknownObject,
+} from "parser";
+import "colors";
+
+class Daph {
+    private commands: ICommand[] = [];
+    private commandExecuted = false;
+    private handleHelp(command: ICommand): Daph {
+        console.info(
+            `\t${command.name.blue} - Help:\n\t` +
+                `${"-".repeat(command.name.length + 9)}\n\t` +
+                `${"Command Name".green}: ${command.name.yellow}\n\t` +
+                `${"Usage".green}: ${command.name.yellow} ${
+                    command.usage.yellow
+                }\n\t` +
+                `${"Aliases".green}: ${command.aliases
+                    .map((alias) => alias.yellow)
+                    .join(", ")}\n\t` +
+                `${"Category".green}: ${command.category.yellow}\n\t` +
+                `${"Description".green}: ${command.description.yellow}\n\t` +
+                `${"Example".green}:\n\t` +
+                `\t${command.example
+                    .map((example) => `${command.name.blue} ${example.yellow}`)
+                    .join("\n\t\t")}`,
+        );
+        return this;
+    }
+    public createCommand(command: ICommand, fn: DaphFunction): Daph {
+        command.example.unshift("--help");
+        this.commands.push(command);
+        if (
+            command.name !== process.argv[2] &&
+            !command.aliases.includes(process.argv[2])
+        )
+            return this;
+        if (this.commandExecuted) return this;
+        this.commandExecuted = true;
+        const args = bargs(
+            command.argDefinitions,
+            process.argv.slice(3),
+        ) as BargsResult;
+        const outputArgs: UnknownObject = {};
+        if (args.help || args._unknown.help || args.h || args._unknown.h)
+            return this.handleHelp(command);
+        for (const definition of command.argDefinitions) {
+            if (!definition.isOptional && !args[definition.name]) return this.handleHelp(command);
+            outputArgs[definition.name] = args[definition.name];
+        }
+        fn(command.name, outputArgs);
+        return this;
+    }
+    public help(): void {
+        if (this.commandExecuted) return;
+        const help: HelpCategory = {};
+        for (const command of this.commands) {
+            const category = command.category;
+            if (!help[category]) help[category] = [];
+            help[category].push(command);
+        }
+        let str = "";
+        for (const category in help) {
+            const categoryName = (
+                category.charAt(0).toUpperCase() + category.slice(1)
+            ).green;
+            const commands = help[category]
+                .map(
+                    (command) =>
+                        `\t${command.name.yellow} ${command.usage.yellow}`,
+                )
+                .join("\n\t");
+            str += `\t${categoryName} Commands:\n\t${"-".repeat(
+                categoryName.length + 1,
+            )}\n\t${commands}\n\n`;
+        }
+        console.info(str);
+    }
+}
+
+const daph = new Daph();
+
+export default daph;
